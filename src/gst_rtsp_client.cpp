3b9397efa74f37b476838b7f9221c371a2b5c6b8
root <root@debian10.toybrick>
2021-10-26 09:41:31 +0800

    mppversion bug fix

--- src/gst_rtsp_client.cpp
+++ src/gst_rtsp_client.cpp
@@ -832,17 +832,17 @@ RtspClient::read(int width, int height) {
   
   // ** 
 
-  int ret;
-  GstVideoMeta *meta = gst_buffer_get_video_meta (buf);
-  guint nplanes = GST_VIDEO_INFO_N_PLANES (&(this->m_data->info));
+  // int ret;
+  // GstVideoMeta *meta = gst_buffer_get_video_meta (buf);
+  // guint nplanes = GST_VIDEO_INFO_N_PLANES (&(this->m_data->info));
   // guint width, height;
   GstMapInfo map_info;
-  gchar filename[128];
-  GstVideoFormat pixfmt;
-  const char *pixfmt_str;
+  // gchar filename[128];
+  // GstVideoFormat pixfmt;
+  // const char *pixfmt_str;
 
-  pixfmt = GST_VIDEO_INFO_FORMAT (&(this->m_data->info));
-  pixfmt_str = gst_video_format_to_string (pixfmt);
+  // pixfmt = GST_VIDEO_INFO_FORMAT (&(this->m_data->info));
+  // pixfmt_str = gst_video_format_to_string (pixfmt);
 
   /* TODO: use the DMABUF directly */
   gst_buffer_map (buf, &map_info, GST_MAP_READ);
@@ -850,15 +850,15 @@ RtspClient::read(int width, int height) {
   int source_width = GST_VIDEO_INFO_WIDTH (&(this->m_data->info));
   int source_height = GST_VIDEO_INFO_HEIGHT (&(this->m_data->info));
 
-  /* output some information at the beginning (= when the first frame is handled) */
-  if (this->m_data->frame == 0) {
-    printf ("===================================\n");
-    printf ("GStreamer video stream information:\n");
-    printf ("  size: %u x %u pixel\n", source_width, source_height);
-    printf ("  pixel format: %s  number of planes: %u\n", pixfmt_str, nplanes);
-    printf ("  video meta found: %s\n", yesno (meta != NULL));
-    printf ("===================================\n");
-  }
+  // /* output some information at the beginning (= when the first frame is handled) */
+  // if (this->m_data->frame == 0) {
+  //   printf ("===================================\n");
+  //   printf ("GStreamer video stream information:\n");
+  //   printf ("  size: %u x %u pixel\n", source_width, source_height);
+  //   printf ("  pixel format: %s  number of planes: %u\n", pixfmt_str, nplanes);
+  //   printf ("  video meta found: %s\n", yesno (meta != NULL));
+  //   printf ("===================================\n");
+  // }
 
   // rga
   rga_buffer_t 	src;
@@ -866,9 +866,9 @@ RtspClient::read(int width, int height) {
   rga_buffer_t  dst_output;
   rga_buffer_t  dst_resize_output;
 
-  g_print("mppframe size : %d \n", map_info.size);
+  // g_print("mppframe size : %d \n", map_info.size);
 
-  g_print("format %f \n",get_bpp_from_format(RK_FORMAT_YCrCb_420_SP));
+  // g_print("format %f \n",get_bpp_from_format(RK_FORMAT_YCrCb_420_SP));
 
   // // 1920*1080
   // // mpp 265 256 2304 | 264 16 1088
